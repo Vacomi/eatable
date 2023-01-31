57b7bcae35612b6d75681cbf764aaff553761483
Michael Vargas <vacomi.corp@gmail.com>
2023-01-31 11:49:22 -0500

    avance vacomi - history

--- src/app/home/history/history.component.ts
+++ src/app/home/history/history.component.ts
@@ -4,6 +4,7 @@ import { OrdersService } from 'src/app/services/orders.service';
 import { SessionService } from 'src/app/services/session.service';
 import { Order } from 'src/app/Models/Orders.models';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/Models/Producto.model';
 
 @Component({
   selector: 'app-history',
@@ -13,36 +14,56 @@ import { ProductService } from 'src/app/services/product.service';
 
 export class HistoryComponent implements OnInit {
   
-  objOrder =  {
-    delivery_address: '',
-  }
+  plato: Product = {
+    _id: "",
+    name: "",
+    price: 0,
+    category: "",
+    description: "",
+    picture_url: "",
+  };
   ide = sessionStorage.getItem('id');
   ordenes:any = [
   ];
   copia2: Order[] = []
 
+  lsproducts = [];
   constructor( private lol : Location, private _sorder : OrdersService, private _suser: SessionService, private _sproduct : ProductService ) {}
 
   ngOnInit(): void {
     this._sorder.getOrders().subscribe( ( orders : any) => {
-      console.log(orders);
       this.copia2 = orders.filter((orden:any) => orden.user_id === this.ide)
       this.ordenes = [...orders];
-      console.warn(this.ordenes);
+
+      this._sproduct.getProducts().subscribe( (data:any) => {
+        this.lsproducts = data; 
+      })
     })
-    // this._sproduct.getProductId
+    // this._sproduct.getProducts().subscribe( (data:any) => {
+    //   this.lsproducts = data; 
+    // })
   }
 
-  
-
-  getPName( id:string ) {
-    this._sproduct.getProductId(id).subscribe((product:any) => product.name);
+  getTotal (id:string | undefined ):any {
+    if(!id) {return  ''}
+    let nuevoord = this.copia2.filter( order => order._id === id )
+    let obj = {...nuevoord[0]};
+    let total = 0;
+    for(let product of obj.items) {
+      total += this.getPrecio(product._id) * product.quantity;
+    }
+    return total;
   }
-  getPPrecio ( id:string ){
-    this._sproduct.getProductId(id).subscribe((product:any) => {
-      console.warn(product);
-      
-    });
+
+  getName( id:string ):any {
+    let pro : Product[] = this.lsproducts.filter( (p:any) => p._id === id)
+    let obj = {...pro[0]};
+    return  obj.name;  
+  }
+  getPrecio ( id:string ){
+    let pro : Product[] = this.lsproducts.filter( (p:any) => p._id === id)
+    let obj = {...pro[0]};
+    return  obj.price;  
   }
 
   back() {
